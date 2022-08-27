1e61b645b91534141588580ccb2dccd8e5126d75
channi3714 <dhckd3714@gmail.com>
2022-08-28 02:15:49 +0900

    Location 2nd commit

--- irion/views.py
+++ irion/views.py
@@ -1,7 +1,11 @@
+from http.client import HTTPResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from django.contrib.auth.models import User
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .serializer import LocationSerializer
 
@@ -15,8 +19,20 @@ class LocationView(APIView):
         serializer = LocationSerializer(locations, many=True)
         return Response(serializer.data)
 
-# def location(request):
+
+def location(request):
+    if request.method == "GET":
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
+        return Response(serializer.data)
+
+# @csrf_exempt
+# def location(request, pk):
+#     try:
+#         locations = Location.objects.get(pk=pk)
+#     except Location.DoesNotExist:
+#         return HttpResponse(status=404)
 #     if request.method == "GET":
-#         locations = Location.objects.all()
+#         # locations = Location.objects.all()
 #         serializer = LocationSerializer(locations, many=True)
-#         return Response(serializer.data)
+#         return JsonResponse(serializer.data)
